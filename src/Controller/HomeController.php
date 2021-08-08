09e57b044ecc90f2989515e77f36bcd3b712914d
Mohamed <mohamed.elquchiri@tbdgroup.com>
2021-08-08 15:23:18 +0200

    third commit

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,7 +9,47 @@ use Symfony\Component\HttpFoundation\Response;
 
 class HomeController extends AbstractController
 {
-    public function hello(): Response {
-        return $this->render('hello.html.twig');
+    public function index(): Response {
+        $data = [
+            [
+                'category' => "Forum de bienvenue",
+                'forums' => [
+                    [
+                        'title' => "Forum du bienvenue",
+                        'updated' => "@mdo",
+                        'count' => "458 . 4587",
+                        'activity' => "Ok",
+                        'icon' => 'clock-o'
+                    ],
+                    [
+                        'title' => "Forum du bienvenue 2",
+                        'updated' => "@mdo",
+                        'count' => "458 . 4587",
+                        'activity' => "Ok",
+                        'icon' => 'check'
+                    ]
+                ]
+            ],
+            [
+                'category' => "Développement informatique",
+                'forums' => [
+                    [
+                        'title' => "Design & Sketching",
+                        'updated' => "@mdo",
+                        'count' => "458 . 4587",
+                        'activity' => "Ok",
+                        'icon' => 'users'
+                    ],
+                    [
+                        'title' => "L'univers PHP",
+                        'updated' => "@mdo",
+                        'count' => "458 . 4587",
+                        'activity' => "Ok",
+                        'icon' => 'code'
+                    ]
+                ]
+            ]
+        ];
+        return $this->render('hello.html.twig', ['categories' => $data]);
     }
 }
